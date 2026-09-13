7cf8cb47e35790f94d4bc86f091dde7a634322e0
theus8491 <mateus.up16@gmail.com>
2026-09-13 15:02:28 -0300

    primeiro commit - assistente pokemom331ww

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'guilda-anomalia-v1';
-const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon.svg'];
+const CACHE_NAME = 'guilda-anomalia-v2';
+const APP_SHELL = ['./', './index.html', './manifest.json', './icons/icon-192.png', './icons/icon-512.png'];
 
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(APP_SHELL)));
